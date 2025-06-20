1dbfbe732198d6749ca2a8852e6bf3d27f6da3be
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-06-20 19:16:26 +0530

    some comment setup

--- src/basic_chat/index.ts
+++ src/basic_chat/index.ts
@@ -9,6 +9,9 @@ const context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
 	},
 ];
 
+// when gpt responses it role is assistant and when i put some data then my role is user and for initial setup we need to set role system and the content.
+// The content helps or how the way gpt will act if you say humor or funny it response will like that to.
+
 async function createChatCompletion() {
 	const response = await openai.chat.completions.create({
 		model: 'gpt-4o',
